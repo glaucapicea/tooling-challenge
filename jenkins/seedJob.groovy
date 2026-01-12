f007ab40d3e3b65a3f66c2aea4bc866ced353c66
sakib <sakibrahman0@gmail.com>
2026-01-11 18:56:37 -0700

    Add jenkins jobdsl script

---
+++ jenkins/seedJob.groovy
@@ -0,0 +1,44 @@
+job('test-health-reporter') {
+
+    description('Daily test suite health report generator')
+
+    parameters {
+        stringParam(
+                'RESULT_FILE',
+                'result.json',
+                'Name of the test result JSON file'
+        )
+    }
+
+    // Scheduled trigger for 9AM PST every day
+    triggers {
+        cron('0 9 * * *')
+    }
+
+    // Absolute timeout of 10 minutes
+    wrappers {
+        timestamps()
+        timeout {
+            absolute(10)
+        }
+    }
+
+    scm {
+        git {
+            remote {
+                url('https://github.com/glaucapicea/tooling-challenge.git')
+            }
+            branch('master')
+        }
+    }
+
+    steps {
+        shell('''
+            echo "Building Test Health CLI..."
+            mvn clean package
+
+            echo "Running Test Health Report..."
+            java -jar target/test-health-cli-1.0.0.jar $RESULT_FILE
+        ''')
+    }
+}
